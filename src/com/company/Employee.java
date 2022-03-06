384b695ddc9e388a2f481712b58a12f42b76c95d
Metody Aleksov <teddy57603@gmail.com>
2022-03-06 20:46:21 +0200

    added getters and setters to the Employee class

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -1,17 +1,41 @@
 package com.company;
 
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
-    public int extraHours;
+    private int baseSalary;
+    private int hourlyRate;
+    private int extraHours;
 
     public Employee(int baseSalary, int hourlyRate, int extraHours){
-        this.extraHours = extraHours;
-        this.baseSalary = baseSalary;
-        this.hourlyRate = hourlyRate;
+        this.setExtraHours(extraHours);
+        this.setBaseSalary(baseSalary);
+        this.setHourlyRate(hourlyRate);
     }
 
     public int calculateWage(){
-        return baseSalary + (hourlyRate * extraHours);
+        return getBaseSalary() + (getHourlyRate() * getExtraHours());
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public int getExtraHours() {
+        return extraHours;
+    }
+
+    public void setExtraHours(int extraHours) {
+        this.extraHours = extraHours;
     }
 }
