997376cc54dad49263f66ab8ac332e2e9d2c04de
Metody Aleksov <teddy57603@gmail.com>
2022-06-01 15:18:20 +0300

    cleaned main method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    var employee = new Employee(50000, 20, 12);
-        int wage = employee.calculateWage();
 
-        System.out.println(wage);
     }
 }
